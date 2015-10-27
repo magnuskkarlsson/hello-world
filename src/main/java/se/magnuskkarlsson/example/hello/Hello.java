98d376d5bebadebf07cb5184c3cda985da0b06b8
magnuskkarlsson <magnus.r.karlsson@gmail.com>
2015-10-27 01:59:28 +0100

    Format Hello

--- src/main/java/se/magnuskkarlsson/example/hello/Hello.java
+++ src/main/java/se/magnuskkarlsson/example/hello/Hello.java
@@ -2,8 +2,7 @@ package se.magnuskkarlsson.example.hello;
 
 public class Hello {
 
-  public static void main(String[] args) {
-    System.out.println("Hello");
-  }
-
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 }
