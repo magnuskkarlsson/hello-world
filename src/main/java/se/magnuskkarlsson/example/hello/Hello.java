1085aa961891673d2b51b163fbfc9a178e2f40a9
magnuskkarlsson <magnus.r.karlsson@gmail.com>
2016-06-02 20:47:29 +0200

    Changed hello again text

--- src/main/java/se/magnuskkarlsson/example/hello/Hello.java
+++ src/main/java/se/magnuskkarlsson/example/hello/Hello.java
@@ -3,6 +3,6 @@ package se.magnuskkarlsson.example.hello;
 public class Hello {
 
     public String hello(String name) {
-        return "Hello there " + name;
+        return "Hello there again" + name;
     }
 }
